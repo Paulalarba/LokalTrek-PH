2d404922f0257ead54111342f93106a6b91b5a50
Paul Alarba <alarbapaulax@gmail.com>
2026-05-03 15:19:26 +0800

    MVC Development Set Up

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LokalTrek_PH.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
